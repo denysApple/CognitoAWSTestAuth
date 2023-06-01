a1b65f2c7d26bf467d03ce3aa0c77ea674233b25
Denys Havryliak <den4iklvivua@gmail.com>
2023-06-01 15:28:31 +0300

    Initial Commit

---
+++ CognitoAWSTestTask/CognitoAWSTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CognitoAWSTestTaskApp.swift
+//  CognitoAWSTestTask
+//
+//  Created by Denys on 01.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CognitoAWSTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
